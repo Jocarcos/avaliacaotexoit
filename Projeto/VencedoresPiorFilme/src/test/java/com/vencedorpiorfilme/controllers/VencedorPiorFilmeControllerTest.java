46086a9fcb859bf968f1b2cf49d9c2fd8ee3fb0a
João Carlos Costa <joaocarlos.costa@outlook.com>
2021-09-08 18:08:05 -0300

    Finalizando Testes

--- Projeto/VencedoresPiorFilme/src/test/java/com/vencedorpiorfilme/controllers/VencedorPiorFilmeControllerTest.java
+++ Projeto/VencedoresPiorFilme/src/test/java/com/vencedorpiorfilme/controllers/VencedorPiorFilmeControllerTest.java
@@ -1,8 +1,8 @@
 package com.vencedorpiorfilme.controllers;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -23,17 +24,53 @@ public class VencedorPiorFilmeControllerTest {
 	public WebApplicationContext context;
 	
 	private MockMvc mvc;
-	
+		
 	@Before
 	public void setup() {
 		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
 	}
 	
 	@Test
-	public void testRequisicaoIndicadosAno() throws Exception{
+	public void testGetIntervalo() throws Exception{
 		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-		String url = "/vencedores/2019"; 
+		String url = "/vencedores/getintervalo"; 
 		this.mvc.perform(get(url))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testGetVencedorAno() throws Exception{
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+		String url = "/vencedores/getvencedorano/2018"; 
+		this.mvc.perform(get(url))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testGetIndicadosAno() throws Exception{
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+		String url = "/vencedores/getindicadosano/2019"; 
+		this.mvc.perform(get(url))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testPost() throws Exception{
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+		String url = "/vencedores/post"; 
+		this.mvc.perform(post(url)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(" {\"year\": 2020,\"title\": \"OutroTeste\",\"studios\": \"Universal Pictures\",\"producers\": \"Teste\",\"winner\": \"yes\"}"))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void testPut() throws Exception{
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+		String url = "/vencedores/put"; 
+		this.mvc.perform(put(url)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(" {\"id\": 207,\"year\": 2020,\"title\": \"OutroTeste\",\"studios\": \"Universal Pictures\",\"producers\": \"Teste\",\"winner\": \"yes\"}"))
 		.andExpect(status().isOk());
 	}
 	
